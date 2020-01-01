d504104411636e31c5f483d5a12de0fb1589883a
lb1wh <dhf@hydra.org>
2020-01-01 16:29:08 +0100

    Split user into handling into separate thread

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+// TODO: Enable GMCP-support (telnet option 201?)
 use std::env;
 use std::io::{self, Read, Write};
-use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, TryRecvError};
 use std::net::TcpStream;
-use std::thread;
 use std::time::Duration;
 use std::fs;
-
 use serde::{Serialize, Deserialize};
+
+mod user_input_thread;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Target {
@@ -45,9 +44,12 @@ fn read_config() -> Config {
 fn read_network(stream: &mut TcpStream, network_in: &mut Vec<u8>) {
     match stream.read(network_in) {
         Ok(_) => {
-            for c in network_in.iter() {
-                print!("{}", *c as char);
-            }
+
+            let input = match std::str::from_utf8(network_in) {
+                Ok(data) => print!("{}", data),
+                Err(e) => print!("[{}]", e),
+            };
+
             io::stdout().flush().unwrap();
             for i in network_in.iter_mut() { *i = 0; }
         }
@@ -55,31 +57,6 @@ fn read_network(stream: &mut TcpStream, network_in: &mut Vec<u8>) {
         // errors produced by read() that require special handling?
         Err(_) => {}
     };
-}
-
-/// Separate thread that reads user (keyboard) input.
-fn spawn_stdin_channel() -> Receiver<String> {
-    let (tx, rx) = mpsc::channel::<String>();
-
-    thread::spawn(move || loop {
-        let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).unwrap();
-        // Send input across the channel, to the owning thread.
-        tx.send(buffer).expect("Unable to transmit");
-    });
-
-    rx
-}
-
-/// Read user (keyboard) input without blocking.
-fn read_user_input(stream: &mut TcpStream, receiver: &Receiver<String>) {
-    match receiver.try_recv() {
-        Ok(user_input) => {
-            stream.write(&user_input.as_bytes()).expect("Unable to transmit");
-        },
-        Err(TryRecvError::Empty) => (), //println!("Error: Channel empty"),
-        Err(TryRecvError::Disconnected) => (), //panic!("Error: Channel disconnected"),
-    }
 }
 
 fn main() {
@@ -91,7 +68,7 @@ fn main() {
         format!("{}:{}", &config.target.hostname, &config.target.port))
         .expect(&format!("Unable to connect to host: {}", &config.target.hostname));
 
-    // Disable blocking for read, write, recv and send
+    // Disable blocking for read, write, recv and send.
     stream.set_nonblocking(true).expect("Call to set_nonblocking failed");
 
     stream.set_read_timeout(Some(Duration::new(config.network.read_timeout, 0)))
@@ -99,12 +76,12 @@ fn main() {
 
     let mut network_in = vec![0; config.network.input_buflen];
     // Receive user input in a separate thread.
-    let stdin_channel = spawn_stdin_channel();
+    let stdin_channel = user_input_thread::spawn_stdin_channel();
 
     // This approach doesn't assume that all inputs end in '\n' or EOF,
     // unlike BufReader and several other reader-functions.
     loop {
         read_network(&mut stream, &mut network_in);
-        read_user_input(&mut stream, &stdin_channel);
+        user_input_thread::read_user_input(&mut stream, &stdin_channel);
     };
 }
---
+++ src/user_input_thread.rs
@@ -0,0 +1,43 @@
+// User input (keyboard) thread.
+use std::io::{self, Write};
+use std::net::TcpStream;
+use std::thread;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, TryRecvError};
+
+/// Spawn a thread that reads user input (keyboard).
+/// The spawning thread receives input to this thread via
+/// the rx end of the channel.
+pub fn spawn_stdin_channel() -> Receiver<String> {
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+
+        if buffer.starts_with("/") {
+            process_command(&buffer);
+        } else {
+            // Send input across the channel, to the owning thread.
+            tx.send(buffer).expect("Unable to transmit");
+        }
+    });
+
+    rx
+}
+
+/// Read user (keyboard) input without blocking, and forward it
+/// to the remote host.
+pub fn read_user_input(stream: &mut TcpStream, receiver: &Receiver<String>) {
+    match receiver.try_recv() {
+        Ok(user_input) => {
+            stream.write(&user_input.as_bytes()).expect("Unable to transmit");
+        },
+        Err(TryRecvError::Empty) => (), //println!("Error: Channel empty"),
+        Err(TryRecvError::Disconnected) => (), //panic!("Error: Channel disconnected"),
+    }
+}
+
+fn process_command(cmd: &str) {
+    println!("process_command does nothing...");
+}
